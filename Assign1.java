3b6d390cf47be2a1d6a5c1fb0ce120ff2bf8220b
pranavmeee <41279386+pranavmeee@users.noreply.github.com>
2018-07-19 04:13:55 -0700

    Add files via upload
    Displays a pattern

---
+++ Assign1.java
@@ -0,0 +1,14 @@
+
+public class Assign1 {
+	public static void main (String args[]) {
+		int a=1,b=10;
+		for (int i=1;i<=10;i++) {
+			for (int j=10; j>0;j--) {
+			}
+				
+				System.out.println(a+ "\t" + b);
+			a++;b--;
+		}
+	}
+
+}
